d4e2300d0d79b27fdc682610dc0173030078d337
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-25 23:32:52 +1000

    Updated ResourcesUtilities.

--- src/tests/H.Modules.UnitTests/Utilities/ResourcesUtilities.cs
+++ src/tests/H.Modules.UnitTests/Utilities/ResourcesUtilities.cs
@@ -10,29 +10,52 @@ namespace H.Modules.UnitTests.Utilities
     public static class ResourcesUtilities
     {
         /// <summary>
-        /// <![CDATA[Version: 1.0.0.1]]>
+        /// Searches for a file among Embedded resources <br/>
+        /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
+        /// <![CDATA[Version: 1.0.0.3]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns></returns>
         public static Stream ReadFileAsStream(string name, Assembly? assembly = null)
         {
+            name = name ?? throw new ArgumentNullException(nameof(name));
             assembly ??= Assembly.GetExecutingAssembly();
 
-            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(name));
-
-            return assembly.GetManifestResourceStream(resourceName) ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources");
+            try
+            {
+                return assembly.GetManifestResourceStream(
+                           assembly
+                               .GetManifestResourceNames()
+                               .Single(resourceName => resourceName.EndsWith(name)))
+                       ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources");
+            }
+            catch (InvalidOperationException exception)
+            {
+                throw new ArgumentException(
+                    "Not a single one was found or more than one resource with the given name was found. " +
+                    "Make sure there are no collisions and the required file has the attribute \"Embedded resource\"", 
+                    exception);
+            }
         }
 
         /// <summary>
-        /// <![CDATA[Version: 1.0.0.1]]>
-        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]>
+        /// Searches for a file among Embedded resources <br/>
+        /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
+        /// <![CDATA[Version: 1.0.0.2]]> <br/>
+        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns></returns>
         public static string ReadFileAsString(string name, Assembly? assembly = null)
         {
+            name = name ?? throw new ArgumentNullException(nameof(name));
+
             using var stream = ReadFileAsStream(name, assembly);
             using var reader = new StreamReader(stream);
 
@@ -40,14 +63,20 @@ namespace H.Modules.UnitTests.Utilities
         }
 
         /// <summary>
-        /// <![CDATA[Version: 1.0.0.1]]>
-        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]>
+        /// Searches for a file among Embedded resources <br/>
+        /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
+        /// <![CDATA[Version: 1.0.0.2]]> <br/>
+        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns></returns>
         public static byte[] ReadFileAsBytes(string name, Assembly? assembly = null)
         {
+            name = name ?? throw new ArgumentNullException(nameof(name));
+
             using var stream = ReadFileAsStream(name, assembly);
             using var memoryStream = new MemoryStream();
 
