4dad074045fc083c86d6f48fd6ac429fa143a84e
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-08 11:51:56 +1000

    Fixed test.

--- src/tests/H.Modules.UnitTests/RealModulesTests.cs
+++ src/tests/H.Modules.UnitTests/RealModulesTests.cs
@@ -12,7 +12,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace H.Modules.UnitTests
 {
     [TestClass]
-    [Ignore]
     public class RealModulesTests
     {
         [TestInitialize]
@@ -35,15 +34,18 @@ namespace H.Modules.UnitTests
                 "H.Notifiers.RssNotifier", 
                 "H.Notifiers.RssNotifier",
                 "RssNotifier",
-                async (instance, token) =>
+                async (instance, cancellationToken) =>
                 {
                     instance.SetSetting("IntervalInMilliseconds", "1000");
                     instance.SetSetting("Url", "https://www.upwork.com/ab/feed/topics/rss?securityToken=3046355554bbd7e304e77a4f04ec54ff90dcfe94eb4bb6ce88c120b2a660a42c47a42de8cfd7db2f3f4962ccb8c9a8d1bb2bff326e55b5b464816c9919c4e66c&userUid=749097038387695616&orgUid=749446993539981313");
                     
-                    await Task.Delay(TimeSpan.FromSeconds(5), token);
+                    await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
 
                     var value = instance.GetModuleVariableValue("$rss_last_title$");
                     Console.WriteLine($"Rss Last Title: {value}");
+
+                    Assert.IsNotNull(value, nameof(value));
+                    Assert.AreNotEqual(string.Empty, value, nameof(value));
                 });
         }
 
@@ -53,9 +55,11 @@ namespace H.Modules.UnitTests
             string shortName,
             Func<IModule, CancellationToken, Task> testFunc) 
         {
-            var receivedException = (Exception?)null;
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(15));
+            var cancellationToken = cancellationTokenSource.Token;
 
+            var receivedException = (Exception?)null;
+            
             await using var manager = new ModuleManager<IModule>(
                 Path.Combine(Path.GetTempPath(), $"H.Containers.Tests_{name}"));
             manager.ExceptionOccurred += (_, exception) =>
@@ -74,11 +78,24 @@ namespace H.Modules.UnitTests
                 typeName, 
                 bytes, 
                 container => container.LaunchInCurrentProcess = true,
-                cancellationTokenSource.Token);
+                cancellationToken);
 
             Assert.IsNotNull(instance);
 
-            var types = await manager.GetTypesAsync(cancellationTokenSource.Token);
+            instance.NewCommand += (_, command) =>
+            {
+                Console.WriteLine($"{nameof(instance.NewCommand)}: {command}");
+            };
+            instance.ExceptionOccurred += (_, exception) =>
+            {
+                Console.WriteLine($"{nameof(instance.ExceptionOccurred)}: {exception}");
+            };
+            instance.LogReceived += (_, log) =>
+            {
+                Console.WriteLine($"{nameof(instance.LogReceived)}: {log}");
+            };
+            
+            var types = await manager.GetTypesAsync(cancellationToken);
             ShowList(types, "Available types");
 
             Assert.AreEqual(shortName, instance.ShortName);
@@ -86,11 +103,11 @@ namespace H.Modules.UnitTests
             var availableSettings = instance.GetAvailableSettings().ToArray();
             ShowList(availableSettings, "Available settings");
 
-            await testFunc(instance, cancellationTokenSource.Token);
+            await testFunc(instance, cancellationToken);
 
             try
             {
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationTokenSource.Token);
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }
             catch (OperationCanceledException)
             {
