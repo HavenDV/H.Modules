7ca4c4f7d5fb646e91042374d4e15dd5919a560a
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-14 03:56:03 +1000

    Added RunnerService.

---
+++ src/libs/H.Services.RunnerService/RunnerService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Services.Core;
+
+namespace H.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class RunnerService : ServiceBase
+    {
+        #region Constructors
+
+        /// <param name="commandProducers"></param>
+        public RunnerService(params ICommandProducer[] commandProducers)
+        {
+            commandProducers = commandProducers ?? throw new ArgumentNullException(nameof(commandProducers));
+
+            foreach (var producer in commandProducers)
+            {
+                producer.CommandReceived += OnCommandReceived;
+
+                Dependencies.Add(producer);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static Task RunAsync(string value, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private async void OnCommandReceived(object _, string value)
+        {
+            try
+            {
+                await RunAsync(value).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (Exception exception)
+            {
+                OnExceptionOccurred(exception);
+            }
+        }
+
+        #endregion
+    }
+}
