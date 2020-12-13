82f25405abc7955884a62051ee18e6effdbe3415
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-14 01:16:09 +1000

    Refactored.

--- src/libs/H.Services.Core/ModuleFinder.cs
+++ src/libs/H.Services.Core/ModuleFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using H.Core.Converters;
 using H.Core.Recorders;
@@ -12,32 +13,35 @@ namespace H.Services.Core
     {
         #region Properties
 
-        private IModuleService ModuleService { get; }
+        private ICollection<IModuleService> ModuleServices { get; }
         
         /// <summary>
         /// 
         /// </summary>
-        public IRecorder Recorder => (IRecorder)ModuleService
-            .Modules
+        public IRecorder Recorder => (IRecorder)ModuleServices
+            .SelectMany(service => service.Modules)
             .First(module => module.ShortName.EndsWith("Recorder", StringComparison.OrdinalIgnoreCase));
 
         /// <summary>
         /// 
         /// </summary>
-        public IConverter Converter => (IConverter)ModuleService
-            .Modules
+        public IConverter Converter => (IConverter)ModuleServices
+            .SelectMany(service => service.Modules)
             .First(module => module.ShortName.EndsWith("Converter", StringComparison.OrdinalIgnoreCase));
 
         #endregion
 
         #region Constructors
 
-        /// <param name="moduleService"></param>
-        public ModuleFinder(IModuleService moduleService)
+        /// <param name="moduleServices"></param>
+        public ModuleFinder(params IModuleService[] moduleServices)
         {
-            ModuleService = moduleService ?? throw new ArgumentNullException(nameof(moduleService));
-            
-            Dependencies.Add(moduleService);
+            ModuleServices = moduleServices ?? throw new ArgumentNullException(nameof(moduleServices));
+
+            foreach (var moduleService in moduleServices)
+            {
+                Dependencies.Add(moduleService);
+            }
         }
 
         #endregion
